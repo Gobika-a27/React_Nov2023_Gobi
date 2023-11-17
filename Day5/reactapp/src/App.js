bb007764542f3127244f0de3905ce628d22bdf35
Gobika-a27 <124855811+Gobika-a27@users.noreply.github.com>
2023-11-17 11:45:31 +0530

    DAY5

---
+++ Day5/reactapp/src/App.js
@@ -0,0 +1,14 @@
+import Lifecycle from './components/Lifecycle';
+import Funlifecycle from './components/Funlifecycle';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Funlifecycle/>
+     <Lifecycle/>
+    </div>
+  );
+}
+
+export default App;
