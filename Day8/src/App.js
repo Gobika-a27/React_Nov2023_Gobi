a2af855735540c5ce3ba920a97e093ca91740700
Gobika-a27 <124855811+Gobika-a27@users.noreply.github.com>
2023-11-23 15:10:29 +0530

    DAY8

---
+++ Day8/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AxiosGet from './components/AxiosGet';
+import AxiosPost from './components/AxiosPost';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AxiosGet/>
+      <AxiosPost/>
+     
+    </div>
+  );
+}
+
+export default App;
